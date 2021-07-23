f077c74c2491f135ed739e342e7b9c9d6a13f0f1
Vyacheslav Poplavsky <avalsoff@gmail.com>
2021-07-23 10:23:56 +1000

    Unit 6: Add primes lib and test it

--- unit-6/primes/src/Primes.hs
+++ unit-6/primes/src/Primes.hs
@@ -1,4 +1,31 @@
 module Primes where
 
+sieve :: [Int] -> [Int]
+sieve [] = []
+sieve (nextPrime:rest) = nextPrime : sieve noFactors
+  where noFactors = filter ((/= 0) . (`mod` nextPrime)) rest
+
+maxSize = 10000
+
 primes :: [Int]
-primes = [1 ..]
+primes = sieve [2 .. maxSize]
+
+isPrime :: Int -> Maybe Bool
+isPrime n
+  | n < 2 = Nothing
+  | n > maxSize = Nothing
+  | otherwise = Just (n `elem` primes)
+
+unsafePrimeFactors :: Int -> [Int] -> [Int]
+unsafePrimeFactors 0 [] = []
+unsafePrimeFactors n [] = []
+unsafePrimeFactors n (next:primes) =
+  if n `mod` next == 0 
+    then next:unsafePrimeFactors (n `div` next) (next:primes)
+    else unsafePrimeFactors n primes
+
+primeFactors :: Int -> Maybe [Int]
+primeFactors n | n < 2 = Nothing 
+               | n > maxSize = Nothing
+               | otherwise = Just (unsafePrimeFactors n primesLessThanN)
+  where primesLessThanN = filter (<= n) primes
